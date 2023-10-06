0e1b7ca55dc59c3ac748a2175d108f2b41122634
JERG002 <julian.ramgarza@gmail.com>
2023-10-05 22:41:32 -0600

    Pruebas ANOVA varias

--- ANOVA.py
+++ ANOVA.py
@@ -1,44 +1,84 @@
 import pandas as pd
 from scipy.stats import f_oneway
-import statsmodels.api as sm
-from statsmodels.multivariate.manova import MANOVA
+
+def resultado(resultadoAnova):
+    if resultadoAnova.pvalue < 0.05:
+        print("Hay diferencias significativas entre al menos dos grupos.")
+    else:
+        print("No hay diferencias significativas entre los grupos.")
 
 datos = pd.read_csv('TransporteNL_Limpio.csv')
-datos['FECHA'] = pd.to_datetime(datos.FECHA, format='%Y-%m-%d %H:%M:%S')
-datos['ANIO'] = datos['FECHA'].dt.year
 
 
-grupo_pasajeros_transportados = [grupo['Pasajeros transportados'].tolist() for _, grupo in datos.groupby('TRANSPORTE')]
-resultado_anova_pasajeros = f_oneway(*grupo_pasajeros_transportados)
-print("Resultados ANOVA para Pasajeros Transportados:")
-print(resultado_anova_pasajeros)
+datosNL = datos.where(datos['ID_ENTIDAD'] == 19)
+datost1 = datosNL.where(datosNL['TRANSPORTE'] == 'Metrorrey')
+datost1 = datost1.dropna(subset='ID_ENTIDAD')
+datost1['Metro'] = datost1['Pasajeros transportados']
+datpst2 = datosNL.where(datosNL['TRANSPORTE'] == 'Transmetro')
+datpst2 = datpst2.dropna(subset='TRANSPORTE')
+datpst2['Transmetro'] = datpst2['Pasajeros transportados']
+datosjoinNL = pd.concat([datost1, datpst2], axis=0, ignore_index=True)
+
+resultado_anova01 = f_oneway(datost1['Metro'], datpst2['Transmetro'])
 
 
-grupo_unidades_operando = [grupo['UNIDADES_OPERANDO'].tolist() for _, grupo in datos.groupby('ID_ENTIDAD')]
-resultado_anova_unidades = f_oneway(*grupo_unidades_operando)
-print("Resultados ANOVA para Unidades Operando:")
-print(resultado_anova_unidades)
+datosCMX = datos.where(datos['ID_ENTIDAD'] == 9)
+datost1 = datosCMX.where(datosCMX['TRANSPORTE'] == 'Sistema de Transporte Colectivo Metro')
+datost1 = datost1.dropna(subset='ID_ENTIDAD')
+datost1['Metro'] = datost1['Pasajeros transportados']
+datpst2 = datosCMX.where(datosCMX['TRANSPORTE'] == 'Metrobús')
+datpst2 = datpst2.dropna(subset='TRANSPORTE')
+datpst2['Metrobus'] = datpst2['Pasajeros transportados']
+datosjoinCDMX = pd.concat([datost1, datpst2], axis=0, ignore_index=True)
 
 
 
-grupo_kilometros_recorridos = [grupo['Kilómetros recorridos'].tolist() for _, grupo in datos.groupby('ANIO')]
-resultado_anova_kilometros = f_oneway(*grupo_kilometros_recorridos)
-print("Resultados ANOVA para Kilómetros Recorridos:")
-print(resultado_anova_kilometros)
-
-datos['Pasajeros'] = datos['Pasajeros transportados']
-datos['Kilometros'] = datos['Kilómetros recorridos']
+resultado_anova1 = f_oneway(datosjoinNL['Pasajeros transportados'], datosjoinNL['UNIDADES_OPERANDO'])
+resultado_anova2 = f_oneway(datosjoinNL['Kilómetros recorridos'], datosjoinNL['UNIDADES_OPERANDO'])
+resultado_anova3 = f_oneway(datost1['Metro'], datpst2['Metrobus'])
+resultado_anova4 = f_oneway(datosjoinCDMX['Pasajeros transportados'], datosjoinCDMX['UNIDADES_OPERANDO'])
 
 
-variables = ['Pasajeros', 'Kilometros', 'UNIDADES_OPERANDO']
-datos['grupo'] = datos['TRANSPORTE']
+print("ANOVA NUEVO LEON Metro - Transmetro: ",resultado_anova01)
+resultado(resultado_anova01)
+print("ANOVA NUEVO LEON Metro y Transmetro - Unidades Operando: ",resultado_anova1)
+resultado(resultado_anova1)
+print("ANOVA NUEVO LEON Kilometros recorridos - Unidades Operando: ",resultado_anova2)
+resultado(resultado_anova2)
+print("ANOVA CDMX Metro - Metrobus: ",resultado_anova3)
+resultado(resultado_anova3)
+print("ANOVA CDMX Metro y Metrobus - Unidades Operando: ",resultado_anova4)
+resultado(resultado_anova4)
 
-manova = MANOVA.from_formula(formula='Pasajeros + Kilometros + UNIDADES_OPERANDO ~ grupo', data=datos)
-resultados_manova = manova.mv_test()
+datosCMX = datos.where(datos['ID_ENTIDAD'] == 9)
+datost1 = datosCMX.where(datosCMX['TRANSPORTE'] == 'Tren Ligero')
+datost1 = datost1.dropna(subset='ID_ENTIDAD')
+datost1['TrenLigero'] = datost1['Pasajeros transportados']
+datpst2 = datosCMX.where(datosCMX['TRANSPORTE'] == 'Trolebús')
+datpst2 = datpst2.dropna(subset='TRANSPORTE')
+datpst2['Trole'] = datpst2['Pasajeros transportados']
+datosjoinCDMX = pd.concat([datost1, datpst2], axis=0, ignore_index=True)
+resultado_anova04 = f_oneway(datost1['TrenLigero'], datpst2['Trole'])
+resultado_anova4 = f_oneway(datosjoinCDMX['Pasajeros transportados'], datosjoinCDMX['UNIDADES_OPERANDO'])
 
-# Muestra los resultados
-print(resultados_manova)
+print("ANOVA CDMX Tren ligero - Trolebus: ",resultado_anova4)
+resultado(resultado_anova4)
+print("ANOVA CDMX Tren ligero y Trolebus - Unidades Operando: ",resultado_anova4)
+resultado(resultado_anova4)
 
-#El resultado del Pvalor en las pruebas ANOVA es muy cercano al 0, por lo tanto las diferencias entre algunas de las medias son estadísticamente significativas
+datosCMX = datos.where(datos['ID_ENTIDAD'] == 9)
+datost1 = datosCMX.where(datosCMX['TRANSPORTE'] == 'Red de Transporte de Pasajeros')
+datost1 = datost1.dropna(subset='ID_ENTIDAD')
+datost1['Bus'] = datost1['Pasajeros transportados']
+datpst2 = datosCMX.where(datosCMX['TRANSPORTE'] == 'Sistema de Transporte Colectivo Metro')
+datpst2 = datpst2.dropna(subset='TRANSPORTE')
+datpst2['Metro'] = datpst2['Pasajeros transportados']
+datosjoinCDMX = pd.concat([datost1, datpst2], axis=0, ignore_index=True)
 
-print("Las diferencias entre las medias SON ESTADISTICAMENTE SIGNIFICATIVAS")
+resultado_anova04 = f_oneway(datost1['Bus'], datpst2['Metro'])
+resultado_anova4 = f_oneway(datosjoinCDMX['Pasajeros transportados'], datosjoinCDMX['UNIDADES_OPERANDO'])
+
+print("ANOVA CDMX Autobus - Metro: ",resultado_anova04)
+resultado(resultado_anova04)
+print("ANOVA CDMX Autobus y Metro - Unidades Operando: ",resultado_anova4)
+resultado(resultado_anova4)
